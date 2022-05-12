0c6d3ccc3d4b7a6f685ce9ff16a9c30f72b0a6b5
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-05-12 01:35:49 -0500

    Add project files.

--- UI/Ventana Menu/VentanaMenu.xaml.cs
+++ UI/Ventana Menu/VentanaMenu.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Tarea_8_LogIn_y_RegistroDeUsuariosEnWpf.UI.Consultas;
 using Tarea_8_LogIn_y_RegistroDeUsuariosEnWpf.UI.Registros;
+using Tarea_8_LogIn_y_RegistroDeUsuariosEnWpf.UI.Ayuda;
 using System.Windows;
 
 namespace Tarea_8_LogIn_y_RegistroDeUsuariosEnWpf.UI.Ventana_Menu
@@ -24,6 +25,12 @@ namespace Tarea_8_LogIn_y_RegistroDeUsuariosEnWpf.UI.Ventana_Menu
         {
             var cUsuario = new cUsuarios();
             cUsuario.Show();
+        }
+
+        private void AyudaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var ayudas = new Ayudas();
+            ayudas.Show();
         }
     }
 }
